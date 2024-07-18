4d9c8007c3e6f8a68cf93acbfcd4750f4e4d5977
julie_c <forechecker@gmail.com>
2024-07-18 13:51:25 -0600

    updates to links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-document.getElementById("year").innerHTML = date.getFullYear();
+//document.getElementById("year").innerHTML = date.getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
